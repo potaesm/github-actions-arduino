f56fc722e16bf6e967a1fcea4706c62b4c5fd7f7
Suthinan Musitmani <suthinan.musitmani@gmail.com>
2022-12-19 01:16:25 +0530

    New build

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,205 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
+/***/ 2753:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+const core = __nccwpck_require__(1590);
+const github = __nccwpck_require__(5824);
+
+const fs = __nccwpck_require__(4686);
+const path = __nccwpck_require__(1017);
+
+const express = __nccwpck_require__(9544);
+const localtunnel = __nccwpck_require__(4051);
+const mqtt = __nccwpck_require__(9585);
+const axios = __nccwpck_require__(5363);
+
+// const mqttConfig = {
+// 	url: 'mqtt://puffin.rmq2.cloudamqp.com',
+// 	options: {
+// 		username: 'gwbvwhzr:gwbvwhzr',
+// 		password: 'BH4UyDm74GHbzdsYJOFtvZL7LTIM_bNB'
+// 	},
+// 	topic: 'main/update'
+// };
+
+const STAGE = {
+	BIN_URL_SENT: 'BIN_URL_SENT',
+	BIN_URL_RECEIVED: 'BIN_URL_RECEIVED',
+	UPDATE_FAILED: 'UPDATE_FAILED',
+	UPDATE_OK: 'UPDATE_OK',
+	TIMEOUT: 'TIMEOUT'
+};
+
+function timeDiff(date1, date2) {
+	const second = 1000,
+		minute = second * 60,
+		hour = minute * 60,
+		day = hour * 24;
+	let ms = Math.abs(date1 - date2);
+	const days = parseInt(String(ms / day), 10);
+	ms -= days * day;
+	const hours = parseInt(String(ms / hour), 10);
+	ms -= hours * hour;
+	const minutes = parseInt(String(ms / minute), 10);
+	ms -= minutes * minute;
+	const seconds = parseInt(String(ms / second), 10);
+	return { days, hours, minutes, seconds };
+}
+
+function openFileServer(binaryPath = '') {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const port = 3001;
+			const app = express();
+			app.use(express.json());
+			app.get('/', (request, response) => response.download(binaryPath));
+			const server = app.listen(port);
+			const tunnel = await localtunnel({ port });
+			// const tunnel = { url: 'localhost' };
+			let retry = 10;
+			while (!!retry) {
+				try {
+					await axios({
+						method: 'get',
+						url: tunnel.url.replace('https://', 'http://')
+					});
+					retry = 0;
+				} catch (error) {
+					retry--;
+				}
+			}
+			return resolve({ server, tunnel });
+		} catch (error) {
+			return reject(error);
+		}
+	});
+}
+
+function closeFileServer(server = __nccwpck_require__(9544)().listen(), tunnel = __nccwpck_require__(4051)()) {
+	return new Promise(async (resolve, reject) => {
+		try {
+			tunnel.close();
+			return server.close((error) => {
+				if (!!error) reject(error);
+				resolve();
+			});
+		} catch (error) {
+			return reject(error);
+		}
+	});
+}
+
+function startDeployment(deployOptions, monitorStage = (stage = '') => {}) {
+	return new Promise((resolve, reject) => {
+		try {
+			const { deviceId, commitId, binUrl, mqttConfig, timeLimit } = deployOptions;
+			const client = mqtt.connect(mqttConfig.url, mqttConfig.options);
+			const timeout = setTimeout(() => {
+				clearTimeout(timeout);
+				client.publish(mqttConfig.topic, null, { retain: true, qos: 2 });
+				client.end();
+				reject(new Error(STAGE.TIMEOUT));
+			}, timeLimit || 120000);
+			client.on('connect', function () {
+				client.subscribe(mqttConfig.topic, function (error) {
+					if (error) {
+						reject(error);
+					} else {
+						client.publish(mqttConfig.topic, JSON.stringify({ id: deviceId, commit: commitId, url: binUrl.replace('https://', 'http://'), stage: STAGE.BIN_URL_SENT }), { retain: true, qos: 2 });
+					}
+				});
+			});
+			client.on('message', function (topic, message) {
+				const { id, commit, stage } = JSON.parse(message.toString() || '{}');
+				if (topic === mqttConfig.topic && id === deviceId && commit === commitId) {
+					monitorStage(stage);
+					if (stage !== STAGE.BIN_URL_SENT && stage !== STAGE.BIN_URL_RECEIVED) {
+						client.publish(mqttConfig.topic, null, { retain: true, qos: 2 });
+						client.end();
+						if (stage === STAGE.UPDATE_OK) {
+							resolve(stage);
+						} else if (`${stage}`.startsWith(STAGE.UPDATE_FAILED)) {
+							reject(new Error(stage));
+						}
+					}
+				}
+			});
+		} catch (error) {
+			return reject(error);
+		}
+	});
+}
+
+function monitorStage(stage = '') {
+	console.log('Deployment stage: ', stage);
+}
+
+(async function () {
+	try {
+		const startDate = new Date();
+		const commitId = github.context.payload.head_commit?.id;
+		const deviceId = core.getInput('deviceId');
+		const binaryBuildPath = core.getInput('binaryBuildPath');
+		const timeLimit = core.getInput('timeLimit');
+		const mqttUrl = core.getInput('mqttUrl');
+		const mqttUsername = core.getInput('mqttUsername');
+		const mqttPassword = core.getInput('mqttPassword');
+		const mqttTopic = core.getInput('mqttTopic');
+		const mqttConfig = {
+			url: mqttUrl,
+			options: {
+				username: mqttUsername,
+				password: mqttPassword
+			},
+			topic: mqttTopic
+		};
+		const buildFiles = await fs.readdir(binaryBuildPath);
+		// console.log('Build files: ', buildFiles);
+		const binaryFileName = buildFiles.find((fileName) => fileName.includes('.bin'));
+		console.log('Binary file name:', binaryFileName);
+		console.log('Opening file server...');
+		const { server, tunnel } = await openFileServer(path.join(binaryBuildPath, binaryFileName));
+		console.log('Binary file is served at', tunnel.url);
+		console.log('Starting deployment...');
+		let result = '';
+		try {
+			result = await startDeployment({ deviceId, commitId, binUrl: tunnel.url, mqttConfig, timeLimit }, monitorStage);
+		} catch (error) {
+			result = error?.message || `${error}`;
+		}
+		// console.log('Deployment result:', result);
+		console.log('Closing file server...');
+		await closeFileServer(server, tunnel);
+		console.log('File server closed');
+		const endDate = new Date();
+		const log = [endDate.toLocaleString('en-GB').split(', ').join(' ')];
+		const { minutes, seconds } = timeDiff(startDate, endDate);
+		log.push(`Total: ${minutes}m ${seconds}s`);
+		log.push('Result: ' + result + os.EOL);
+		console.log('Summary:', log.join(', '));
+		if (result === STAGE.UPDATE_OK) {
+			return core.setOutput('result', result);
+		}
+		return core.setFailed(new Error(result));
+	} catch (error) {
+		return core.setFailed(error);
+	}
+})();
+
+module.exports = {
+	STAGE,
+	openFileServer,
+	closeFileServer,
+	startDeployment,
+	monitorStage,
+	timeDiff
+};
+
+
+/***/ }),
+
 /***/ 8914:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -64853,172 +65052,12 @@ module.exports = JSON.parse('[[[0,44],"disallowed_STD3_valid"],[[45,46],"valid"]
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
 /******/ 	
 /************************************************************************/
-var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
-(() => {
-const core = __nccwpck_require__(1590);
-const github = __nccwpck_require__(5824);
-
-const fs = __nccwpck_require__(4686);
-const path = __nccwpck_require__(1017);
-
-const express = __nccwpck_require__(9544);
-const localtunnel = __nccwpck_require__(4051);
-const mqtt = __nccwpck_require__(9585);
-const axios = __nccwpck_require__(5363);
-
-// const mqttConfig = {
-// 	url: 'mqtt://puffin.rmq2.cloudamqp.com',
-// 	options: {
-// 		username: 'gwbvwhzr:gwbvwhzr',
-// 		password: 'BH4UyDm74GHbzdsYJOFtvZL7LTIM_bNB'
-// 	},
-// 	topic: 'main/update'
-// };
-
-const STAGE = {
-	BIN_URL_SENT: 'BIN_URL_SENT',
-	BIN_URL_RECEIVED: 'BIN_URL_RECEIVED',
-	UPDATE_FAILED: 'UPDATE_FAILED',
-	UPDATE_OK: 'UPDATE_OK',
-	TIMEOUT: 'TIMEOUT'
-};
-
-function openFileServer(binaryPath = '') {
-	return new Promise(async (resolve, reject) => {
-		try {
-			const port = 3001;
-			const app = express();
-			app.use(express.json());
-			app.get('/', (request, response) => response.download(binaryPath));
-			const server = app.listen(port);
-			const tunnel = await localtunnel({ port });
-			// const tunnel = { url: 'localhost' };
-			let retry = 10;
-			while (!!retry) {
-				try {
-					await axios({
-						method: 'get',
-						url: tunnel.url.replace('https://', 'http://')
-					});
-					retry = 0;
-				} catch (error) {
-					retry--;
-				}
-			}
-			return resolve({ server, tunnel });
-		} catch (error) {
-			return reject(error);
-		}
-	});
-}
-
-function closeFileServer(server = __nccwpck_require__(9544)().listen(), tunnel = __nccwpck_require__(4051)()) {
-	return new Promise(async (resolve, reject) => {
-		try {
-			tunnel.close();
-			return server.close((error) => {
-				if (!!error) reject(error);
-				resolve();
-			});
-		} catch (error) {
-			return reject(error);
-		}
-	});
-}
-
-function startDeployment(deployOptions, monitorStage = (stage = '') => {}) {
-	return new Promise((resolve, reject) => {
-		try {
-			const { deviceId, commitId, binUrl, mqttConfig, timeLimit } = deployOptions;
-			const client = mqtt.connect(mqttConfig.url, mqttConfig.options);
-			const timeout = setTimeout(() => {
-				clearTimeout(timeout);
-				client.publish(mqttConfig.topic, null, { retain: true, qos: 2 });
-				client.end();
-				reject(new Error(STAGE.TIMEOUT));
-			}, timeLimit || 120000);
-			client.on('connect', function () {
-				client.subscribe(mqttConfig.topic, function (error) {
-					if (error) {
-						reject(error);
-					} else {
-						client.publish(mqttConfig.topic, JSON.stringify({ id: deviceId, commit: commitId, url: binUrl.replace('https://', 'http://'), stage: STAGE.BIN_URL_SENT }), { retain: true, qos: 2 });
-					}
-				});
-			});
-			client.on('message', function (topic, message) {
-				const { id, commit, stage } = JSON.parse(message.toString() || '{}');
-				if (topic === mqttConfig.topic && id === deviceId && commit === commitId) {
-					monitorStage(stage);
-					if (stage !== STAGE.BIN_URL_SENT && stage !== STAGE.BIN_URL_RECEIVED) {
-						client.publish(mqttConfig.topic, null, { retain: true, qos: 2 });
-						client.end();
-						if (stage === STAGE.UPDATE_OK) {
-							resolve(stage);
-						} else if (`${stage}`.startsWith(STAGE.UPDATE_FAILED)) {
-							reject(new Error(stage));
-						}
-					}
-				}
-			});
-		} catch (error) {
-			return reject(error);
-		}
-	});
-}
-
-function monitorStage(stage = '') {
-	console.log('Deployment stage: ', stage);
-}
-
-(async function () {
-	try {
-		const commitId = github.context.payload.head_commit?.id;
-		const deviceId = core.getInput('deviceId');
-		const binaryBuildPath = core.getInput('binaryBuildPath');
-		const timeLimit = core.getInput('timeLimit');
-		const mqttUrl = core.getInput('mqttUrl');
-		const mqttUsername = core.getInput('mqttUsername');
-		const mqttPassword = core.getInput('mqttPassword');
-		const mqttTopic = core.getInput('mqttTopic');
-		const mqttConfig = {
-			url: mqttUrl,
-			options: {
-				username: mqttUsername,
-				password: mqttPassword
-			},
-			topic: mqttTopic
-		};
-		const buildFiles = await fs.readdir(binaryBuildPath);
-		console.log('Build files: ', buildFiles);
-		const binaryFileName = buildFiles.find((fileName) => fileName.includes('.bin'));
-		console.log('Binary file name: ', binaryFileName);
-		console.log('Opening file server...');
-		const { server, tunnel } = await openFileServer(path.join(binaryBuildPath, binaryFileName));
-		console.log('Binary file is served at ', tunnel.url);
-		console.log('Starting deployment...');
-		let result = '';
-		try {
-			result = await startDeployment({ deviceId, commitId, binUrl: tunnel.url, mqttConfig, timeLimit }, monitorStage);
-		} catch (error) {
-			result = error?.message || `${error}`;
-		}
-		console.log('Deployment result: ', result);
-		console.log('Closing file server...');
-		await closeFileServer(server, tunnel);
-		console.log('File server closed');
-		if (result === STAGE.UPDATE_OK) {
-			return core.setOutput('result', result);
-		}
-		return core.setFailed(new Error(result));
-	} catch (error) {
-		return core.setFailed(error);
-	}
-})();
-
-})();
-
-module.exports = __webpack_exports__;
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __nccwpck_require__(2753);
+/******/ 	module.exports = __webpack_exports__;
+/******/ 	
 /******/ })()
 ;
